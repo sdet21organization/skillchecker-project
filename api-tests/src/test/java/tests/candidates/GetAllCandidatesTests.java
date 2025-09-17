7f29bc1e2db9ebdd03fcb817b8419b178ea52c49
Dmytro Pavlusenko <pavlusenko.de@gmail.com>
2025-09-17 18:02:40 +0200

    Auth solution is updated

---
+++ api-tests/src/test/java/tests/candidates/GetAllCandidatesTests.java
@@ -0,0 +1,20 @@
+package tests.candidates;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import tests.BaseTest;
+import wrappers.Candidates;
+
+@DisplayName("Get all candidates tests")
+public class GetAllCandidatesTests extends BaseTest {
+
+    @Test
+    @DisplayName("Check getting all candidates")
+    void getAllCandidates () {
+        Response response = Candidates.getAllCandidates(cookie);
+        Candidates.verifySuccessfulGetAllCandidatesResponse(response);
+
+
+}
+}
