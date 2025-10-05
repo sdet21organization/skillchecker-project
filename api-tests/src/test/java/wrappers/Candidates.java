62c5421d9422e46f0455aa1ebc906c8bf65e0833
Dmytro Pavlusenko <pavlusenko.de@gmail.com>
2025-10-05 10:34:25 +0200

    ss-64 added 5 new api TCs for verifying import candidates requests

--- api-tests/src/test/java/wrappers/Candidates.java
+++ api-tests/src/test/java/wrappers/Candidates.java
@@ -12,19 +12,16 @@ public class Candidates {
 
     @Step("Get all candidates")
     public static Response getAllCandidates(String cookie) {
-        Response response = given().contentType("application/json").header("Cookie", cookie).when().get("candidates").then().assertThat().contentType(ContentType.JSON).extract().response();
-        return response;
+        return  given().contentType("application/json").header("Cookie", cookie).when().get("candidates").then().assertThat().contentType(ContentType.JSON).extract().response();
     }
 
     @Step("Create a new Candidate")
     public static Response createNewCandidate(String cookie, CandidatesRequest payload) {
-        Response response = given().contentType("application/json").header("Cookie", cookie).body(payload).when().post("candidates").then().assertThat().contentType(ContentType.JSON).extract().response();
-        return response;
+        return given().contentType("application/json").header("Cookie", cookie).body(payload).when().post("candidates").then().assertThat().contentType(ContentType.JSON).extract().response();
     }
 
     @Step("Import Candidates")
     public static Response importCandidates(String cookie, ImportCandidatesRequest payload) {
-        Response response = given().contentType("application/json").header("Cookie", cookie).body(payload).when().post("candidates/import").then().assertThat().contentType(ContentType.JSON).extract().response();
-        return response;
+        return given().contentType("application/json").header("Cookie", cookie).body(payload).when().post("candidates/import").then().assertThat().contentType(ContentType.JSON).extract().response();
     }
 }
